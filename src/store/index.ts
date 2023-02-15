1db6bb11c870244de9d5614c3119b4a5f162e784
Kabarbum <misha.pro2856@gmail.com>
2023-02-16 00:39:05 +0500

    disable logger

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,7 +5,7 @@ import {firestoreReducer} from 'redux-firestore'
 import {fillingReducer} from "./slices/filling/filling.slice";
 import {productReducer} from "@/store/slices/product/products.slice";
 import {adminReducer} from "@/store/slices/admin/admin.slice";
-import logger from "redux-logger";
+// import logger from "redux-logger";
 
 const rootReducer = combineReducers({
     products: productReducer,
@@ -17,8 +17,8 @@ const rootReducer = combineReducers({
 
 export const store = configureStore({
     reducer: rootReducer,
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(logger)
+    // middleware: (getDefaultMiddleware) =>
+    //     getDefaultMiddleware().concat(logger)
 })
 
 export type TypeDispatch = typeof store.dispatch
