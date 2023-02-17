3fd2593f59bf26e0897d058397b9b4d4ba5c1ec9
Kabarbum <misha.pro2856@gmail.com>
2023-02-17 22:39:07 +0500

    превью ссылки в соц. сетях

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -4,6 +4,7 @@ import {FC, ReactNode} from "react";
 import Footer from "@/components/Footer";
 import Image from 'next/image'
 import background from '@/assets/img/image.jpg'
+import cart from '@/assets/img/cart.jpg'
 
 
 interface MainContainerProps {
@@ -23,7 +24,15 @@ const MainContainer:FC<MainContainerProps> = ({children}) => {
                     content="купить торты Питер,бенто,капкейки,трафйлы,чизкейк,купить торты,торт на свадьбу Питер,торт красный бархат,кондитер"
                 />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href={"/favicon.ico"} />
+                <link rel="icon" href={"/favicon.ico"}/>
+                <meta property="og:title" content="Торты Питер на заказ"/>
+                <meta property="og:site_name" content="Торты Питер"/>
+                <meta property="og:url" content="vkysnotort.spb.ru"/>
+                <meta property="og:description" content="Торты на заказ по вашим пожеланиям в Питере!
+                  Чизкейки, трайфлы, бенто и капкейки с любимой начинкой."/>
+                <meta property="og:image" content={cart}/>
+                <meta property="og:image:width" content="1200"/>
+                <meta property="og:image:height" content="1200"/>
             </Head>
             <div className="App">
                 <Navbar/>
